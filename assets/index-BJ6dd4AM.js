5e4798d8534685addfbdf804452bfb3c0c7c3e31
Vitaliy Smirnov <temmets20@gmail.com>
2024-03-05 23:27:53 +0700

    Updates

---
+++ assets/index-BJ6dd4AM.js
@@ -0,0 +1,1 @@
+import{u as a,j as s,R as r}from"./index-CKL6gBd6.js";import{A as n,T as o,a as p}from"./text-goeUAo05.js";import{B as u}from"./index-Bk5wdrnr.js";const c=()=>{const t=a(),e=()=>{t(r.QUIZ)};return s.jsxs(s.Fragment,{children:[s.jsx(n,{src:"presents.mp3",play:!0}),s.jsx(o,{paragraph:p.PRESENTS}),s.jsx(u,{onClick:e,title:"Далее"})]})};export{c as default};
---
+++ assets/index-BYKQ_QRy.js
@@ -0,0 +1,1 @@
+import{u as r,j as t,R as e}from"./index-CKL6gBd6.js";import{A as o,T as n,a as u}from"./text-goeUAo05.js";import{B as i}from"./index-Bk5wdrnr.js";const c=()=>{const a=r(),s=()=>{a(e.ONBOARDING)};return t.jsxs(t.Fragment,{children:[t.jsx(o,{src:"start.mp3",play:!0}),t.jsx(n,{paragraph:u.START}),t.jsx(i,{onClick:s,title:"Далее"})]})};export{c as default};
