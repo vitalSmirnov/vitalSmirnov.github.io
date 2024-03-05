5d8e8f25aaf324e59594551a6b51d3f4360023fb
Vitaliy Smirnov <temmets20@gmail.com>
2024-03-05 21:45:30 +0700

    main.tsx

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,13 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from '../shared/routes/appRoutes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <AppRoutes />
+  </BrowserRouter>
 )
