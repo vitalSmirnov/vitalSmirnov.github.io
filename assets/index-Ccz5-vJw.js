544c4cf1e7a4ac9818c75d71c0174b3b07793558
Vitaliy Smirnov <temmets20@gmail.com>
2024-03-07 05:18:24 +0700

    Updates

---
+++ assets/index-Ccz5-vJw.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DEpukrmC.js";const e="_buttonContainer_1kccz_1",r="_button_1kccz_1",n={buttonContainer:e,button:r},u=({onClick:o,title:s})=>t.jsx(t.Fragment,{children:t.jsx("div",{className:n.buttonContainer,children:t.jsx("button",{onClick:o,className:n.button,children:s})})});export{u as B};
---
+++ assets/index-oJYhvH1Z.js
@@ -0,0 +1,1 @@
+import{u as a,j as s,R as r}from"./index-DEpukrmC.js";import{A as n,T as o,a as p}from"./text-BPSda-V4.js";import{B as u}from"./index-Ccz5-vJw.js";const c=()=>{const t=a(),e=()=>{t(r.QUIZ)};return s.jsxs(s.Fragment,{children:[s.jsx(n,{src:"presents.mp3",play:!0}),s.jsx(o,{paragraph:p.PRESENTS}),s.jsx(u,{onClick:e,title:"Далее"})]})};export{c as default};
